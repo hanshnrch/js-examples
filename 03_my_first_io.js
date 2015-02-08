966974cbbd0ddf0b73daa5e8dfb67867810621fb
Hans-Heinrich <hanshnrch@gmail.com>
2015-02-08 16:31:15 +0100

    nodeschool HH

--- 03_my_first_io.js
+++ 03_my_first_io.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
 var fileName = process.argv.slice(2)[0];
-console.log(fileName);
-var contentBuffer = fs.readFileSyn(fileName);
+//console.log(fileName);
+var contentBuffer = fs.readFileSync(fileName);
+console.log(contentBuffer.toString().split('\n').length -1);
